aaeb66be4e3b643ba13121e055547e4b70e8c4ad
CaoMeiYouRen <996881204@qq.com>
2021-01-11 00:21:39 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+const projectRootDir = path.resolve(__dirname);
+export default defineConfig({
+    server: {
+        port: 4000,
+        open: true,
+        proxy: {}
+    },
+    plugins: [vue()],
+    alias: {
+        '@': path.resolve(projectRootDir, 'src')
+    }
+})
